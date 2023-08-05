cd7833b55fc30216bbd7e298895ae1e47b1ab819
thecow275 <5759030+thecow275@users.noreply.github.com>
2023-08-05 17:41:08 +0300

    Added RMC Only Data used by the RankColor Module

---
+++ Module:RankColor⁄data.lua
@@ -0,0 +1,51 @@
+return {
+ ['Wanderer'] = {
+  rankcolor = '#AAAAAA;',
+  bracketcolor = '#555555;',
+},
+ ['Citizen'] = {
+  rankcolor = '#55FF55;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Gold Citizen'] = {
+  rankcolor = '#FFAA00;',
+  bracketcolor = '#FFAA00;',
+},
+ ['Hero'] = {
+  rankcolor = '#00AA00;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Legend'] = {
+  rankcolor = '#5555FF;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Mystic'] = {
+  rankcolor = '#55FFFF;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Donator'] = {
+  rankcolor = '#FF5555;',
+  bracketcolor = '#555555;',
+},
+ ['Donator+'] = {
+  rankcolor = '#FF5555;',
+  bracketcolor = '#555555;',
+  rankpluscolor = '#AA0000',
+},
+ ['Trial Helper'] = {
+  rankcolor = '#55FF55;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Helper'] = {
+  rankcolor = '#00AAAA;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Moderator'] = {
+  rankcolor = '#FFAA00;',
+  bracketcolor = '#FFFFFF;',
+},
+ ['Admin'] = {
+  rankcolor = '#AA0000;',
+  bracketcolor = '#FFFFFF;',
+},
+}
