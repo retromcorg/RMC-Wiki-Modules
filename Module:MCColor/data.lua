b19389aed60c7e2b739646b5b3a7e53b257e6b2b
Evil Gabe <5759030+thecow275@users.noreply.github.com>
2023-09-01 00:11:10 +0300

    Create data.lua
    Added the data file for the module

---
+++ Module:MCColor/data.lua
@@ -0,0 +1,146 @@
+return {
+ ['&1'] = {
+  wikicolor = '#0000AA;',
+},
+ ['u00A71'] = {
+  wikicolor = '#0000AA;',
+},
+['dark_blue'] = {
+  wikicolor = '#0000AA;',
+},
+ ['&2'] = {
+  wikicolor = '#00AA00;',
+},
+ ['u00A72'] = {
+  wikicolor = '#00AA00;',
+},
+['dark_green'] = {
+  wikicolor = '#00AA00;',
+},
+['&3'] = {
+  wikicolor = '#00AAAA;',
+},
+['u00A73'] = {
+  wikicolor = '#00AAAA;',
+},
+['dark_aqua'] = {
+  wikicolor = '#00AAAA;',
+},
+['&4'] = {
+  wikicolor = '#AA0000;',
+},
+['u00A74'] = {
+  wikicolor = '#AA0000;',
+},
+['dark_red'] = {
+  wikicolor = '#AA0000;',
+},
+['&5'] = {
+  wikicolor = '#AA00AA;',
+},
+['u00A75'] = {
+  wikicolor = '#AA00AA;',
+},
+['dark_purple'] = {
+  wikicolor = '#AA00AA;',
+},
+['&6'] = {
+  wikicolor = '#FFAA00;',
+},
+['u00A76'] = {
+  wikicolor = '#FFAA00;',
+},
+['gold'] = {
+  wikicolor = '#FFAA00;',
+},
+['&7'] = {
+  wikicolor = '#AAAAAA;',
+},
+['u00A77'] = {
+  wikicolor = '#AAAAAA;',
+},
+['gray'] = {
+  wikicolor = '#AAAAAA;',
+},
+['&8'] = {
+  wikicolor = '#555555;',
+},
+['u00A78'] = {
+  wikicolor = '#555555;',
+},
+['dark_gray'] = {
+  wikicolor = '#555555;',
+},
+['&9'] = {
+  wikicolor = '#5555FF;',
+},
+['u00A79'] = {
+  wikicolor = '#5555FF;',
+},
+['blue'] = {
+  wikicolor = '#5555FF;',
+},
+['&a'] = {
+  wikicolor = '#55FF55;',
+},
+['u00A7a'] = {
+  wikicolor = '#55FF55;',
+},
+['green'] = {
+  wikicolor = '#55FF55;',
+},
+['&b'] = {
+  wikicolor = '#55FFFF;',
+},
+['u00A7b'] = {
+  wikicolor = '#55FFFF;',
+},
+['aqua'] = {
+  wikicolor = '#55FFFF;',
+},
+['&c'] = {
+  wikicolor = '#FF5555;',
+},
+['u00A7c'] = {
+  wikicolor = '#FF5555;',
+},
+['red'] = {
+  wikicolor = '#FF5555;',
+},
+['&d'] = {
+  wikicolor = '#FF55FF;',
+},
+['u00A7d'] = {
+  wikicolor = '#FF55FF;',
+},
+['light_purple'] = {
+  wikicolor = '#FF55FF;',
+},
+['&e'] = {
+  wikicolor = '#FFFF55;',
+},
+['u00A7e'] = {
+  wikicolor = '#FFFF55;',
+},
+['yellow'] = {
+  wikicolor = '#FFFF55;',
+},
+['&f'] = {
+  wikicolor = '#FFFFFF;',
+},
+['u00A7f'] = {
+  wikicolor = '#FFFFFF;',
+},
+['white'] = {
+  wikicolor = '#FFFFFF;',
+},
+['&0'] = {
+  wikicolor = '#000000;',
+},
+['u00A70'] = {
+  wikicolor = '#000000;',
+},
+['black'] = {
+  wikicolor = '#000000;',
+},
+ }
